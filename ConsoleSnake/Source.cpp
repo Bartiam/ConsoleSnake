15136e6d7966d237fc49adcbed15a9b55019e3ff
Andrei Goliachenko <itsmyliaf342@yandex.ru>
2023-01-29 01:35:17 +0500

    Добавлена змейка и фрукт, а так же функция Setup(), устанавливающая изначальные установки.

--- ConsoleSnake/Source.cpp
+++ ConsoleSnake/Source.cpp
@@ -1,18 +1,80 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-
 const int height = 26;
-const int Width = 100;
-int x, y, score, snakeIndex;
+const int width = 100;
+int score;
+bool gameOver;
+
+enum Dir {
+	STOP = 0, LEFT, RIGHT, UP, DOWN
+};
 
 void Setup() {
-	
+	score = 0;
+	gameOver = false;
 }
 
-void Draw() {
-	for (int i = 0; i < Width; i++)
+class Snake {
+private:
+	int snakeX;
+	int snakeY;
+public:
+	Snake() {
+		snakeX = height / 2 - 1;
+		snakeY = width / 2 - 1;
+	}
+
+	void setSnakeX(int snakeX) {
+		this->snakeX = snakeX;
+	}
+
+	void setSnakeY(int snakeY) {
+		this->snakeY = snakeY;
+	}
+
+	int getSnakeX() {
+		return snakeX;
+	}
+
+	int getSnakeY() {
+		return snakeY;
+	}
+};
+
+class Fruit {
+private:
+	int fruitX;
+	int fruitY;
+
+public:
+	Fruit() {
+		fruitX = rand() % height - 1;
+		fruitY = rand() % width - 1;
+	}
+
+	void setFruitX(int fruitX) {
+		this->fruitX = fruitX;
+	}
+
+	void setFruitY() {
+		this->fruitY = fruitY;
+	}
+
+	int getFruitX() {
+		return fruitX;
+	}
+
+	int getFruitY() {
+		return fruitY;
+	}
+};
+
+void Draw(Snake &snake, Fruit &fruit) {
+	
+	for (int i = 0; i < width; i++)
 	{
 		cout << "#";
 	}
@@ -21,9 +83,17 @@ void Draw() {
 
 	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < Width; j++) {
-			if (j == 0 || j == Width -1) {
+		for (int j = 0; j < width; j++) {
+			if (j == 0 || j == width -1) {
 				cout << "#";
+			}
+			else if (i == snake.getSnakeX() && j == snake.getSnakeY())
+			{
+				cout << "@";
+			}
+			else if (i == fruit.getFruitX() && j == fruit.getFruitY())
+			{
+				cout << "F";
 			}
 			else {
 				cout << " ";
@@ -32,21 +102,25 @@ void Draw() {
 		cout << endl;
 	}
 
-	for (int i = 0; i < Width; i++)
+	for (int i = 0; i < width; i++)
 	{
 		cout << "#";
 	}
+	
+	cout << endl << score << endl;
+
 	system("cls");
 }
 
 
 
 int main() {
-	
-	
+	srand(time(NULL));
+	Snake snake;
+	Fruit fruit;
 	while (true)
 	{
-		Draw();
+		Draw(snake, fruit);
 	}
 
 
